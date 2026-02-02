b90056288fef0f9cc41140f7e6c7d9742480be1f
Manisha <manishakatika642@gmail.com>
2026-02-02 20:10:12 +0530

    Create valid_parenthesis_string.py

---
+++ valid_parenthesis_string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        n = len(s)
+        low = high = 0
+        for i in range(n):
+            if s[i] == "(":
+                low += 1
+                high += 1
+            elif s[i] == ")":
+                low -= 1
+                high -= 1
+            else:
+                low -= 1
+                high += 1
+            if high < 0:
+                return False
+            low = max(low, 0)
+        return low == 0
